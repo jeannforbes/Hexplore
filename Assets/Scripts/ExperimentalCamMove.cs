35fc786bcfecd5c8131b1df5e3e8eb85575b5bd6
txy9233 <txy9233@rit.edu>
2016-10-17 01:31:57 -0400

    Experimental Camera script created
    New script attached to "Canera_Top" that makes it slightly trail behind
    the movement of the player.
    
    To re-enable the old camera functionality, in the Inspector, select
    "Camera_Top" and uncheck the new script. Then, re-enable the "Camera
    Manager" under "Game Manager".

---
+++ Assets/Scripts/ExperimentalCamMove.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExperimentalCamMove : MonoBehaviour {
+
+    public GameObject player;
+	public Vector3 offset = new Vector3(0, +20, -20);
+
+	void Start(){
+		transform.position = player.transform.position + offset;
+		transform.LookAt (player.transform.position);
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		//set camera somewhere between the two positions, gives a lagging behind or trailing effect. I think. 
+		transform.position = Vector3.Lerp (transform.position, player.transform.position + offset, 0.1f); 
+		//transform.position = player.transform.position + offset;
+	}
+}
